cb92d2d60cb3632ec6d82e72d587263c486a0f4e
Emma-jp-main <mathlogue3431@gmail.com>
2026-05-24 17:22:26 +0900

    feat: main.tsxの作成

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+/*
+BrowserRouter：URLの変更を監視し、対応するコンポーネントを表示する。
+Routes：複数のルートをまとめるためのコンテナ。
+Route：pathに一致するURLが呼ばれると、指定したコンポーネントを表示します。
+ */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<StockList />} />
+                {/*<Route path="/stocks/new" element={<StockInputFrom />} />*/}
+                {/*<Route path="/trade/new" element={<TradeInputForm />} />*/}
+            </Route>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
